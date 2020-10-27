5df5d249d375dc178468c744cba4da8c7565d579
ericmcgrandle <ericmcgrandle@gmail.com>
2020-10-26 20:09:34 -0700

    Confirm.js, Status.js

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,8 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 //BUTTON
 storiesOf("Button", module)
@@ -169,5 +171,17 @@ storiesOf("Button", module)
         interviewer={interviewers[0].name}
         onEdit={action("onEdit")}
         onDelete={action("onDelete")}
+      />
+    )
+    .add("Confirm", () => 
+      <Confirm
+        message="Delete the appointment"
+        onConfirm={action("onConfirm")}
+        onCancel={action("onCancel")}
+      />
+    )
+    .add("Status", () => 
+      <Status
+        message="Deleting"
       />
     )
