5b3784c3265bc1bf1795810be6e88b55069cda4d
ericmcgrandle <ericmcgrandle@gmail.com>
2020-10-28 18:05:00 -0700

    transition, back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
 
 export default function useVisualMode(initial) {
-
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  console.log('mode', mode);
+  function transition(newMode, skip) {
+    setMode(newMode);
 
-  return { mode };
+    if (skip) {
+      const arr = [...history];
+      arr.pop();
+      setHistory([...arr, mode]);  
+    } else {
+      setHistory([...history, newMode]);
+    }
+  }
 
+  function back() {
+    if (history.length === 1) {
+      return;
+    }
+    const arr = [...history];
+    arr.pop();
+    setHistory(arr);
+    setMode(arr[arr.length-1]);
+  }
+
+  return { mode, transition, back };
 };
